eb1e6ff13a43b6967555dcd4c0535eeff9a57c12
MASIDUR RAHAMAN <hello.masidur.dev@gmail.com>
2025-10-03 12:39:19 +0000

    operation

--- 01_basics/03_conversatonoperation.js
+++ 01_basics/03_conversatonoperation.js
@@ -27,8 +27,16 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 34
 
 let stringNumber = String(someNumber)
+
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+
+
+
+
+
+
 
 
 
---
+++ 01_basics/03_operation.js
@@ -0,0 +1,29 @@
+
+
+let value = 3;
+let negValue = -value;
+console.log(negValue);
+
+let str1 =  "hello";
+let str2 = " masidur";
+let str3 = str1 + str2 ;
+console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+// console.log(+"");
+
+
+let gameChanger = 100;
+++gameChanger;
+console.log(gameChanger);
+
+
+
+
+
+
