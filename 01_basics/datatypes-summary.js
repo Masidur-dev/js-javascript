92570fdb1bcd68491f29175324247d90168081a6
MASIDUR RAHAMAN <hello.masidur.dev@gmail.com>
2025-10-03 21:01:00 +0000

    data types summary for interview

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,56 @@
+
+
+// primtive datatypes
+
+
+// 7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt 
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false 
+const outSiteTemp = null 
+let userEmail;
+
+
+const id = Symbol ('123')
+const anotherId = ('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 73587387874878478n;
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+
+// array>>
+const heros = ["shaktiman" , "nagraj", "doga"]
+
+// object
+let myObg = {
+    name :"masidur",
+    age : 22
+};
+
+
+// function 
+
+const myFunction = function () {
+   console.log("hello world");
+   
+}
+
+// *** function data types object function
+
+console.log(typeof bigNumber);
+
+
+
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+
