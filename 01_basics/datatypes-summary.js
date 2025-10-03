6c37a099117e2fdd714fa451827a1eca00f9aabf
MASIDUR RAHAMAN <hello.masidur.dev@gmail.com>
2025-10-03 22:23:50 +0000

    update data types summary js

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -5,18 +5,18 @@
 
 // 7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt 
 
-const score = 100
-const scoreValue = 100.3
+// const score = 100
+// const scoreValue = 100.3
 
-const isLoggedIn = false 
-const outSiteTemp = null 
-let userEmail;
+// const isLoggedIn = false 
+// const outSiteTemp = null 
+// let userEmail;
 
 
-const id = Symbol ('123')
-const anotherId = ('123')
+// const id = Symbol ('123')
+// const anotherId = ('123')
 
-console.log(id === anotherId);
+// console.log(id === anotherId);
 
 // const bigNumber = 73587387874878478n;
 
@@ -27,30 +27,62 @@ console.log(id === anotherId);
 
 
 // array>>
-const heros = ["shaktiman" , "nagraj", "doga"]
+// const heros = ["shaktiman" , "nagraj", "doga"]
 
 // object
-let myObg = {
-    name :"masidur",
-    age : 22
-};
+// let myObg = {
+//     name :"masidur",
+//     age : 22
+// };
 
 
 // function 
 
-const myFunction = function () {
-   console.log("hello world");
+// const myFunction = function () {
+//    console.log("hello world");
    
-}
+// }
 
 // *** function data types object function
 
-console.log(typeof bigNumber);
+// console.log(typeof bigNumber);
 
 
 
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
 
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+  //   Stack (Primitive) ,  Heap (Non-Primtive)
+
+
+  let myYoutubeName = "hiteshchoudharydotcom"
+
+  let anotherName = myYoutubeName
+  anotherName = "chaiaurcode"
+ 
+  console.log(myYoutubeName);
+  
+ console.log(anotherName);
+
+
+ let userOne = {
+    email: "user@goole.com" ,
+      upi: "user@ybl"
+ }
+
+ let userTwo = userOne
+
+ userTwo.email = "hitesh@google.com"
+
+ console.log(userOne.email);
+ console.log(userTwo.email);
+ 
+ 
+ 
 
 
