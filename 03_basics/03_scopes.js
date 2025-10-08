8f79c76f1396b818d69ee5ce8235adf30466e5ee
MASIDUR RAHAMAN <hello.masidur.dev@gmail.com>
2025-10-08 20:59:02 +0000

    scope and mini hoistin

--- 03_basics/03_scopes.js
+++ 03_basics/03_scopes.js
@@ -5,13 +5,63 @@ let a = 400
 if(true){
     let a = 10;
     const b = 20;
-    console.log("inner =", a);  
+    // console.log("inner =", a);  
 }        // its a block scope 
 
 
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+// ///////////////////////////////////////////////////////////////////////////
+
+function one() {
+    const username = "hitesh"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);  
+    }
+    // console.log(website);
+    
+    two()
+    
+}
+
+// one()
+
+if(true) {
+   const username = "hitesh"
+   if(username === "hitesh"){
+    const website = " youtube"
+    // console.log(username + website);
+    
+   }
+//    console.log(website);
+   
+}
+
+// console.log(username);
+
+
+// ++++++++++++++++++++++++++++++++ intersting +++++++++++++++++++++++++++++++++
+
+console.log(addone(5));
+
+function addone(num){
+    return num+1
+}
+
+
+
+addTwo()  // error occurd here 
+const addTwo = function(num){
+    return num + 2
+}
+
+
+
 
 
