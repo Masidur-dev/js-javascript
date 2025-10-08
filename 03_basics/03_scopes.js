2ce558e091cf36a96b61ed3b607d7e649400a416
MASIDUR RAHAMAN <hello.masidur.dev@gmail.com>
2025-10-08 19:51:03 +0000

    block & global scope

---
+++ 03_basics/03_scopes.js
@@ -0,0 +1,17 @@
+
+// var c = 200
+let a = 400
+
+if(true){
+    let a = 10;
+    const b = 20;
+    console.log("inner =", a);  
+}        // its a block scope 
+
+
+
+console.log(a);
+// console.log(b);
+// console.log(c);
+
+
