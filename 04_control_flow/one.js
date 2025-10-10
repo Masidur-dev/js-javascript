92e398eba829d2939aeb66d31e45c4462809c8d3
MASIDUR RAHAMAN <hello.masidur.dev@gmail.com>
2025-10-10 20:20:04 +0000

    control flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,58 @@
+// if 
+
+const isUserLoggedIn = true
+
+// const temperature = 43
+
+// if (temperature <50 ){
+//     console.log("temperature less 50");  
+// }else{
+// console.log("temperatue greatter than 50");
+// }
+
+// console.log("execute");
+
+
+
+// if(2 === "2"){
+//     console.log('execute');
+    
+// }
+
+//  <, >, <=, >=, == , ===, !=
+
+//  var keyword completly global
+
+// const score = 200
+
+// if(score > 100){
+//     let power = "fly"
+//     console.log(`user power: ${power}`);
+  
+// }
+
+// console.log(`user power: ${power}`);
+
+
+//  short hand >>> 
+// const balance = 1000
+// if (balance > 500) console.log("test"); // implecit
+
+
+//  NESTING >>>>
+
+
+// if( balance <500){
+//     console.log("less than 500");
+// }else if(balance<750) {
+//     console.log("less than 750");
+// }else if(balance<900){
+//     console.log("less than 900");
+// }else {
+//     console.log("less than 1200"); 
+    
+// }
+
+
+
+
