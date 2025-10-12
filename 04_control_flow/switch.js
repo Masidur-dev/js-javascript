43b1ab587bbc462359b818d3b0ccb808d068ba89
MASIDUR RAHAMAN <hello.masidur.dev@gmail.com>
2025-10-12 21:29:16 +0000

    control flow

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,66 @@
+
+
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+// const month = 3
+
+const month = "march"
+
+switch (month) {
+    case 1:
+        console.log("jan");
+        
+        break;
+    case 2:
+        console.log("feb");
+        
+        break;
+    case 3:
+        console.log("march");
+        
+        break;
+    case 4:
+        console.log("april");
+        
+        break;
+
+    default:
+        console.log("default");
+    
+        break;
+}
+
+switch (month) {
+    case "jan":
+        console.log("jan");
+        
+        break;
+    case "feb":
+        console.log("feb");
+        
+        break;
+    case "march":
+        console.log("march");
+        
+        break;
+    case "april":
+        console.log("april");
+        
+        break;
+
+    default:
+        break;
+}
+
+//  if nothing match default print
+//  if break cooment after macth everything print except deafault 
+
